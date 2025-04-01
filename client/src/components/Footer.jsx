36d0b17c2dd8f80bee36ba6dd96519f6d8a24cf1
jamesl0350 <locklear.james.jl@gmail.com>
2025-04-01 08:07:13 -0400

    footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>Task Tracker App &copy; {new Date().getFullYear()}</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
